41968987cfafc40cd8a6d30e0e02c58d480e7c32
LimEyez <domaales@gmail.com>
2022-10-08 04:21:21 +0400

    second commit

---
+++ application/Application.php
@@ -0,0 +1,28 @@
+<?php
+require 'db/DB.php';
+require 'user/User.php';
+require 'converts/Convert.php';
+
+class Application {
+    function __construct() {
+        $db = new DB();
+        $this->user = new User($db);
+        $this->convert = new Convert();
+    }
+
+    function login($params) {
+        if ($params['login'] && $params['password']) {
+            return $this->user->login($params['login'], $params['password']);
+        }
+    }
+
+    function convert($params) {
+        $user = $this->user->getUser($params['token']);
+        if ($user && $params['number']) {
+            return $this->convert->converted(
+                $params['number'], 
+                $params['sys'] 
+            );
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+error_reporting(-1);
+
+require('application/Application.php');
+
+function router($params)
+{
+    $method = $params['method'];
+    if ($method) {
+        $app = new Application();
+        switch ($method) {
+            case 'login':
+                return $app->login($params);
+            case 'convert':
+                return $app->convert($params);
+                //...
+        }
+    }
+    return false;
+}
+
+function answer($data)
+{
+    if ($data) {
+        return array(
+            'result' => 'ok',
+            'data' => $data
+        );
+    }
+    return array('result' => 'error');
+}
+
+echo json_encode(answer(router($_GET)));
